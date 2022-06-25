a09cb7ef9b85a03ef4eb862ef19ae7511f02d196
Munchicken <spierce@austin.rr.com>
2022-06-25 11:58:35 -0500

    added my pygame template

--- spritesheet-loader/spritesheet-loader/spritesheet_loader.py
+++ spritesheet-loader/spritesheet-loader/spritesheet_loader.py
@@ -1,4 +1,35 @@
 # Spritesheet Loader
 # Loads images from a spritesheet for use with my games
-# by Sarah Pierce
-print("Spritesheet Loader")
+
+#imports
+import pygame
+
+#initilize game engine
+pygame.init()
+
+#constants
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+#initilize display
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  #moved this to game class last time
+pygame.display.set_caption("Spritesheet Loader")  #moved this to game class last time
+
+#initialize game variables
+is_game_over = False  #moved this to game class last time, in game loop method
+
+#game loop
+#moved this to game class last time, in game loop method
+while not is_game_over:
+
+    #event handler
+    for event in pygame.event.get():
+        #game quit
+        if event.type == pygame.QUIT:
+            is_game_over = True
+
+        #update display
+        pygame.display.update()
+
+#quit game
+pygame.quit()
