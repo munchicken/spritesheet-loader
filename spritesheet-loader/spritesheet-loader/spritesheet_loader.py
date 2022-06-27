24e823333c35cc0b80705e84689f4cebe78919ed
Munchicken <spierce@austin.rr.com>
2022-06-27 12:07:56 -0500

    Moved spritesheet loader.  Added function to create a surface to grab the frame from.

--- spritesheet-loader/spritesheet-loader/spritesheet_loader.py
+++ spritesheet-loader/spritesheet-loader/spritesheet_loader.py
@@ -12,9 +12,17 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 BACKGROUND_COLOR = (50,50,50)  #grey
 
+#functions
+#grab the frame from the spritesheet (w/h of frame on sheet)
+def get_frame(width,height):
+    image = pygame.Surface((width,height)).convert_alpha()
+    return image
+
 #initilize display
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  #moved this to game class last time
 pygame.display.set_caption("Spritesheet Loader")  #moved this to game class last time
+spritesheet_image = pygame.image.load("Fox_idle.png").convert_alpha()  #load spritesheet image
+frame = get_frame(96,96)
 
 #initialize game variables
 is_game_over = False  #moved this to game class last time, in game loop method
@@ -22,15 +30,12 @@ is_game_over = False  #moved this to game class last time, in game loop method
 #game loop
 #moved this to game class last time, in game loop method
 while not is_game_over:
-
-    #load spritesheet image
-    spritesheet_image = pygame.image.load("Fox_idle.png").convert_alpha()
     
     #clear screen
     screen.fill(BACKGROUND_COLOR)
 
-    #display image
-    screen.blit(spritesheet_image, (0,0))
+    #display frame
+    screen.blit(frame, (0,0))
     
     #event handler
     for event in pygame.event.get():
